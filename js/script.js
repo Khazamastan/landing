f2faff17b3f1eaa979e81018afb1c06168f6819c
khaja <khazamastan@gmail.com>
2017-03-17 14:17:00 +0530

    dropdown and landing page

---
+++ js/script.js
@@ -0,0 +1,15 @@
+ $(window).scroll(function () {
+       if ($(window).scrollTop() > ($(window).innerHeight()-60)) {
+         $('nav').addClass('navbar-fixed-top');
+       }
+       if ($(window).scrollTop() < ($(window).innerHeight()-60)) {
+         $('nav').removeClass('navbar-fixed-top');
+       }
+   });
+   $(document).on('click', '#goto-about', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+   });
